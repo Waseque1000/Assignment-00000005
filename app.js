34d9cae9d77f062d6d140be041f1309fe531596b
Waseque Arafat <waseque.ndc@gmail.com>
2024-09-23 17:23:09 +0600

    project start

---
+++ app.js
@@ -0,0 +1,50 @@
+document
+  .getElementById("donate-money-btn")
+  .addEventListener("click", function name(params) {
+    const donatefield = document.getElementById("donate-money");
+    const newDonateAmountField = donatefield.value;
+    const newDonateAmount = parseFloat(newDonateAmountField);
+    donatefield.value = " "; // ! vield empty kore dilam
+    if (isNaN(newDonateAmount)) {
+      alert("Please provide a valid number");
+      return;
+    }
+    {
+      my_modal_1.showModal();
+    }
+  });
+
+//  onclick="my_modal_1.showModal()"
+// console.log("ok");
+
+document
+  .getElementById("donate-money-btn2")
+  .addEventListener("click", function name(params) {
+    const donatefield = document.getElementById("donate-money2");
+    const newDonateAmountField = donatefield.value;
+    const newDonateAmount = parseFloat(newDonateAmountField);
+    donatefield.value = " "; // ! vield empty kore dilam
+    if (isNaN(newDonateAmount)) {
+      alert("Please provide a valid number");
+      return;
+    }
+    {
+      my_modal_2.showModal();
+    }
+  });
+
+document
+  .getElementById("donate-money-btn3")
+  .addEventListener("click", function name(params) {
+    const donatefield = document.getElementById("donate-money3");
+    const newDonateAmountField = donatefield.value;
+    const newDonateAmount = parseFloat(newDonateAmountField);
+    donatefield.value = " "; // ! vield empty kore dilam
+    if (isNaN(newDonateAmount)) {
+      alert("Please provide a valid number");
+      return;
+    }
+    {
+      my_modal_3.showModal();
+    }
+  });
